2faed083b2b489d25261a3a1f74c7f125b5472da
Vaibhavpisute <itzz.vaii17@gmail.com>
2025-08-14 22:48:19 +0530

    Update indexs.sql

--- sql_scripts/indexs.sql
+++ sql_scripts/indexs.sql
@@ -1,6 +1,5 @@
 USE inventory_db;
 
--- Indexes for products table (most frequently queried)
 CREATE INDEX idx_product_name ON products(product_name);
 CREATE INDEX idx_product_code ON products(product_code);
 CREATE INDEX idx_category_id ON products(category_id);
@@ -8,24 +7,19 @@ CREATE INDEX idx_supplier_id ON products(supplier_id);
 CREATE INDEX idx_stock_status ON products(current_stock, minimum_stock);
 CREATE INDEX idx_active_products ON products(is_active);
 
--- Composite indexes for common query patterns
 CREATE INDEX idx_category_stock ON products(category_id, current_stock);
 CREATE INDEX idx_supplier_active ON products(supplier_id, is_active);
 
--- Indexes for stock_movements table
 CREATE INDEX idx_product_movement ON stock_movements(product_id, movement_date);
 CREATE INDEX idx_movement_type ON stock_movements(movement_type);
 CREATE INDEX idx_movement_date ON stock_movements(movement_date);
 CREATE INDEX idx_reference_number ON stock_movements(reference_number);
 
--- Indexes for suppliers table
 CREATE INDEX idx_supplier_name ON suppliers(supplier_name);
 CREATE INDEX idx_supplier_email ON suppliers(email);
 
--- Indexes for categories table
 CREATE INDEX idx_category_name ON categories(category_name);
 
--- Show all indexes
 SELECT 
     TABLE_NAME,
     INDEX_NAME,
